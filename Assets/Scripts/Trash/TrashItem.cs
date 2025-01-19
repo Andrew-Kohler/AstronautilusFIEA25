8c36c502bcd06a15fe6c09dd5796dd9fe202ccc6
Andrew Kohler <awk480@gmail.com>
2025-01-19 10:34:47 -0500

    Updated trash

--- Assets/Scripts/Trash/TrashItem.cs
+++ Assets/Scripts/Trash/TrashItem.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class TrashItem : MonoBehaviour
 {
     [SerializeField] private int pointVal;
     public GameObject trashSound;
-
-    [Header("Options")]
-    [SerializeField] List<GameObject> junks = new List<GameObject>();
+    public bool disappear;
+    public float disTime = 5f;
     void Start()
     {
         //GameManager.Instance.gameActive = true;
-        int index = Random.Range(0, junks.Count);
-        junks[index].SetActive(true);
+        /*int index = Random.Range(0, junks.Count);
+        junks[index].SetActive(true);*/
+        if (disappear)
+        {
+            StartCoroutine(DoShrink());
+        }
     }
 
     // Update is called once per frame
@@ -31,5 +35,20 @@ public class TrashItem : MonoBehaviour
             GameManager.Instance.AddToScore(pointVal * GameManager.Instance.currentMult);
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator DoShrink()
+    {
+        float timer = disTime;
+        while(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            float scaleVal = math.remap(disTime, 0, 1, 0, timer);
+            Debug.Log(scaleVal);
+            this.transform.localScale = new Vector3(scaleVal, scaleVal, scaleVal);
+            yield return null;
+        }
+        Destroy(gameObject);
+
     }
 }
