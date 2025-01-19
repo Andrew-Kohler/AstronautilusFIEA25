4eb51720cbd31a0aac0ceabf616dfab1bcd720c5
Andrew Kohler <awk480@gmail.com>
2025-01-19 02:01:32 -0500

    Fixed trash configs

--- Assets/Scripts/Trash/TrashItem.cs
+++ Assets/Scripts/Trash/TrashItem.cs
@@ -6,9 +6,14 @@ public class TrashItem : MonoBehaviour
 {
     [SerializeField] private int pointVal;
     public GameObject trashSound;
+
+    [Header("Options")]
+    [SerializeField] List<GameObject> junks = new List<GameObject>();
     void Start()
     {
         //GameManager.Instance.gameActive = true;
+        int index = Random.Range(0, junks.Count);
+        junks[index].SetActive(true);
     }
 
     // Update is called once per frame
